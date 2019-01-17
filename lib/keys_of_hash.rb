50318fa2cc3f71fa57faf2db762624dd1875cbd4
JESSICA A BETTS <jessicaannbettsis@gmail.com>
2019-01-17 21:36:31 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -5,6 +5,10 @@ class Hash
       puts "#{data}"
       result.push(self.key(data))
     end
+    
+    self.each do |key,value|
+      puts "#{key} and #{value}"
+    end
     return result
   end
 end
